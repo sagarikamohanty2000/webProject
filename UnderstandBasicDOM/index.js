cfc2ebf5503e65a8a0609011e4d25a3e447ac537
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2023-05-30 17:11:37 +0530

    task2DOM

---
+++ UnderstandBasicDOM/index.js
@@ -0,0 +1,15 @@
+console.dir(document);
+var headerMainTitle = document.getElementById('main-header');
+headerMainTitle.style.borderBottom ='solid 3px #000';
+
+document.getElementById('title_1').style.color ='green';
+var items = document.getElementsByClassName('list-group-item');
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';  
+}
+
+
+   
+    
+
