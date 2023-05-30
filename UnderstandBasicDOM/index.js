eb4b63b974958e5029bfd819051fdea1c4b5c323
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2023-05-30 18:00:33 +0530

    DOM_Task2

--- UnderstandBasicDOM/index.js
+++ UnderstandBasicDOM/index.js
@@ -1,13 +1,16 @@
-console.dir(document);
+
 var headerMainTitle = document.getElementById('main-header');
 headerMainTitle.style.borderBottom ='solid 3px #000';
 
 document.getElementById('title_1').style.color ='green';
+document.getElementById('title_1').style.fontWeight='bold';
 var items = document.getElementsByClassName('list-group-item');
 for(var i=0; i<items.length; i++)
 {
-    items[i].style.fontWeight = 'bold';  
+    items[i].style.fontWeight = 'bold'; 
+    items[i].style.color ='bold'; 
 }
+items[2].style.backgroundColor ='green';
 
 
    
