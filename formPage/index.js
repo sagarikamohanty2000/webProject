e5836801b669afd5a116e3b2cdbcddeb8ab4f64c
Sagarika Mohanty <sagarikamohanty.official@gmail.com>
2023-06-02 01:33:38 +0530

    DOM_Task10

--- formPage/index.js
+++ formPage/index.js
@@ -13,8 +13,23 @@ function myFunction(event)
              userDetail.set('Date',document.getElementById('fDate').value);
              userDetail.set('Time',document.getElementById('fTime').value);
 
+             var ulTag = document.getElementById('user');
+             var newli = document.createElement('li');
+             newli.className='list-user'; //add className
+
+             var nam = document.getElementById('fName').value;
+             var email = document.getElementById('fEmail').value;
+             var phone = document.getElementById('fPhone').value;
+                
+             newli.appendChild(document.createTextNode(nam));
+             newli.appendChild(document.createTextNode('-'));
+             newli.appendChild(document.createTextNode(email));
+             newli.appendChild(document.createTextNode('-'));
+             newli.appendChild(document.createTextNode(phone));
+             ulTag.appendChild(newli);
+           
              var user_serialized = JSON.stringify(Array.from(userDetail));
-             localStorage.setItem("UserDetail",user_serialized);
+             localStorage.setItem(`${email}`,user_serialized);
         }
     
 
